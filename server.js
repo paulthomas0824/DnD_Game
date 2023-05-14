2c09e07a1647f45b04730adda81be067acc79357
paulthomas0824 <paulthomas0824@gmail.com>
2023-05-14 13:29:09 -0500

    Changed things up

--- server.js
+++ server.js
@@ -1,57 +1,40 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-
+const bodyParser = require('body-parser');
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://paulthomas0824:Pc4ever!@cluster0.0eg5u5k.mongodb.net/myDatabase?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || 'ongodb+srv://paulthomas0824:Pc4ever!@cluster0.0eg5u5k.mongodb.net/myDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
 
 app.post('/signup', async (req, res) => {
-    const { email, password } = req.body;
+  const {email, password} = req.body;
 
-    const user = new User({
-        email: email,
-        password: bcrypt.hashSync(password, 8) // Hash the password before saving it
-    });
+  const user = new User({email, password});
+  await user.save();
 
-    try {
-        await user.save();
-        res.json({ message: 'User created successfully' });
-    } catch (error) {
-        res.json({ message: 'An error occurred' });
-    }
+  res.sendStatus(201);
 });
 
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+  const {email, password} = req.body;
 
-    try {
-        const user = await User.findOne({ email: email });
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
+  const user = await User.findOne({email, password});
+  if (!user) {
+    return res.sendStatus(401);
+  }
 
-        const passwordIsValid = bcrypt.compareSync(password, user.password);
-        if (!passwordIsValid) {
-            return res.status(401).json({ auth: false, token: null });
-        }
-
-        // If login is successful, create a token or a session
-        res.json({ message: 'Login successful' });
-    } catch (error) {
-        res.json({ message: 'An error occurred' });
-    }
+  res.sendStatus(200);
 });
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
