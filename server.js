8e20c2f0ab3341d6edae1096040293312e334f1f
paulthomas0824 <paulthomas0824@gmail.com>
2023-05-15 16:11:00 -0500

    added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
-const mongoURI = "mongodb+srv://paulthomas0824:<password>@cluster0.0eg5u5k.mongodb.net/?retryWrites=true&w=majority"; // This will come from your Heroku environment variables
+const mongoURI = "mongodb+srv://paulthomas0824:Pc4ever!@cluster0.0eg5u5k.mongodb.net/myDatabase?retryWrites=true&w=majority"; // This will come from your Heroku environment variables
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
